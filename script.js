ba5192ae37c92603b3ed8465e1e75d9734ab9de2
kaiserinn <azharizqullah90@gmail.com>
2024-03-03 06:06:05 +0800

    fix: ray diagrams untuk cermin dan lensa

--- script.js
+++ script.js
@@ -47,25 +47,34 @@ for (let i = 0; i < inputElements.length; i++) {
     objHeightInput.value = objHeight.value;
     focalLengthInput.value = focalLength.value;
     update();
-  });
+  })
 }
 
 const typeSelections = document.querySelectorAll("input[name='type']");
 for (let i = 0; i < typeSelections.length; i++) {
   typeSelections[i].addEventListener("input", () => {
     update();
-  });
+  })
 }
 
 const simSelections = document.querySelectorAll("input[name='sim']");
 for (let i = 0; i < typeSelections.length; i++) {
   simSelections[i].addEventListener("input", () => {
     update();
-  });
+  })
 }
 
-const menu = document.getElementById("checkbox-menu");
+const menu = document.getElementById('checkbox-menu');
 menu.addEventListener("input", () => update());
+
+const drawCircle = (ctx, cx, cy, r) => {
+  for (let x = cx + r; x >= cx + r - 50; x -= 0.5) {
+    yPos = Math.sqrt(r ** 2 - (x - cx) ** 2) + cy;
+    yNeg = -Math.sqrt(r ** 2 - (x - cx) ** 2) + cy;
+    ctx.fillRect(x, yPos, 1, 1);
+    ctx.fillRect(x, yNeg, 1, 1);
+  }
+}
 
 /**
  * @param {CanvasRenderingContext2D} ctx 2d rendering context
@@ -75,7 +84,14 @@ menu.addEventListener("input", () => update());
  * @param {number} y2 Titik y akhir
  * @param {{color: string, style: "normal" | "dashed"}} [{color="black", style="normal"}={color:"black", style:"normal"}]
  */
-const dda = (ctx, x1, y1, x2, y2, { color = "black", style = "normal" } = { color: "black", style: "normal" }) => {
+const dda = (
+  ctx,
+  x1,
+  y1,
+  x2,
+  y2,
+  { color = "black", style = "normal" } = { color: "black", style: "normal" }
+) => {
   x1 = X_ORIGIN + x1;
   y1 = Y_ORIGIN - y1;
   x2 = X_ORIGIN + x2;
@@ -99,13 +115,13 @@ const dda = (ctx, x1, y1, x2, y2, { color = "black", style = "normal" } = { colo
       if (j % 10 === 0) {
         noPixel = !noPixel;
       }
-      j++;
+      j++
       if (noPixel) {
-        continue;
+        continue
       }
     }
     ctx.fillRect(x1, y1, 1, 1);
-    j++;
+    j++
   }
 };
 
@@ -115,7 +131,7 @@ const dda = (ctx, x1, y1, x2, y2, { color = "black", style = "normal" } = { colo
  * @param {number} y1 Titik y awal
  * @param {number} x2 Titik x akhir
  * @param {number} y2 Titik y akhir
- * @param {{color: string, isVirtual: bool, style: "normal" | "dashed"}} [{color="black", isVirtual=false, style="normal"}={color:"black", isVirtual:false, style:"normal"}]
+ * @param {{color: string, style: "normal" | "dashed", negative: false}} [{color="black", isVirtual=false, style="normal"}={color:"black", style:"normal", negative:false}]
  */
 const ddaRay = (
   ctx,
@@ -123,10 +139,9 @@ const ddaRay = (
   y1,
   x2,
   y2,
-  { color = "black", style = "normal", isVirtual = false } = {
+  { color = "black", style = "normal", negative = false } = {
     color: "black",
     style: "normal",
-    isVirtual: false,
   }
 ) => {
   x1 = X_ORIGIN + x1;
@@ -146,93 +161,23 @@ const ddaRay = (
   let i = 0;
   let noPixel = false;
   while (true) {
-    if (!isVirtual) {
-      if (x1 <= 0 || y1 <= 0) {
-        break;
-      }
-    } else {
-      if (x1 >= CANVAS_WIDTH || y1 >= CANVAS_HEIGHT) {
-        break;
-      }
+    if (x1 <= 0 || y1 <= 0 || x1 >= CANVAS_WIDTH || y1 >= CANVAS_HEIGHT) {
+      break;
     }
 
-    x1 += xIncrement;
-    y1 += yIncrement;
+    x1 = negative ? x1 - xIncrement : x1 + xIncrement;
+    y1 = negative ? y1 - yIncrement : y1 + yIncrement;
     if (style === "dashed") {
       if (i % 10 === 0) {
         noPixel = !noPixel;
       }
-      i++;
+      i++
       if (noPixel) {
         continue;
       }
     }
     ctx.fillRect(x1, y1, 1, 1);
-    i++;
-  }
-};
-
-// fungsi sementara
-const ddaRayMod = (
-  ctx,
-  x1,
-  y1,
-  x2,
-  y2,
-  { color = "black", style = "normal", isVirtual = false } = {
-    color: "black",
-    style: "normal",
-    isVirtual: false,
-  }
-) => {
-  x1 = X_ORIGIN + x1;
-  y1 = Y_ORIGIN - y1;
-  x2 = X_ORIGIN + x2;
-  y2 = Y_ORIGIN - y2;
-
-  const dx = x2 - x1;
-  const dy = y2 - y1;
-  const step = Math.max(Math.abs(dx), Math.abs(dy));
-
-  const xIncrement = dx / step;
-  const yIncrement = dy / step;
-
-  ctx.fillStyle = color;
-
-  let i = 0;
-  let noPixel = false;
-  while (true) {
-    if (!isVirtual) {
-      if (x1 <= 0 || y1 <= 0) {
-        break;
-      }
-    } else {
-      if (x1 >= CANVAS_WIDTH || y1 >= CANVAS_HEIGHT) {
-        break;
-      }
-    }
-
-    if (Math.abs(x1) <= Math.abs(x2) || Math.abs(y1) <= Math.abs(y2)) {
-      if (style === "normal") {
-        style = "dashed";
-      } else if (style === "dashed") {
-        style = "normal";
-      }
-    }
-
-    x1 += xIncrement;
-    y1 += yIncrement;
-    if (style === "dashed") {
-      if (i % 10 === 0) {
-        noPixel = !noPixel;
-      }
-      i++;
-      if (noPixel) {
-        continue;
-      }
-    }
-    ctx.fillRect(x1, y1, 1, 1);
-    i++;
+    i++
   }
 };
 
@@ -244,35 +189,31 @@ const drawBase = (ctx) => {
   dda(ctx, -CANVAS_WIDTH / 2, 0, CANVAS_WIDTH / 2, 0);
 
   dda(ctx, 0, -CANVAS_HEIGHT / 2, 0, CANVAS_HEIGHT + 100);
-};
+}
 
 /**
  * @param {CanvasRenderingContext2D} ctx 2d rendering context
  */
 const drawObject = (ctx) => {
   dda(ctx, -d_o * SCALE, 0, -d_o * SCALE, h_o * SCALE, { color: "red" });
-  dda(ctx, -0.3 * h_o * SCALE + -d_o * SCALE, 0.8 * h_o * SCALE, 0.3 * h_o * SCALE + -d_o * SCALE, 0.8 * h_o * SCALE, {
-    color: "red",
-  });
-  dda(ctx, -0.3 * h_o * SCALE + -d_o * SCALE, 0.8 * h_o * SCALE, -d_o * SCALE, h_o * SCALE, { color: "red" });
-  dda(ctx, 0.3 * h_o * SCALE + -d_o * SCALE, 0.8 * h_o * SCALE, -d_o * SCALE, h_o * SCALE, { color: "red" });
-  dda(ctx, -0.3 * h_o * SCALE + -d_o * SCALE, 0.8 * h_o * SCALE, -d_o * SCALE, 0, { color: "red" });
-  dda(ctx, 0.3 * h_o * SCALE + -d_o * SCALE, 0.8 * h_o * SCALE, -d_o * SCALE, 0, { color: "red" });
-};
+  dda(ctx, (-0.3 * h_o * SCALE) + (-d_o * SCALE), 0.8 * h_o * SCALE, 0.3 * h_o * SCALE + (-d_o * SCALE), 0.8 * h_o * SCALE, { color: "red" });
+  dda(ctx, (-0.3 * h_o * SCALE) + (-d_o * SCALE), 0.8 * h_o * SCALE, -d_o * SCALE, h_o * SCALE, { color: "red" });
+  dda(ctx, 0.3 * h_o * SCALE + (-d_o * SCALE), 0.8 * h_o * SCALE, -d_o * SCALE, h_o * SCALE, { color: "red" });
+  dda(ctx, (-0.3 * h_o * SCALE) + (-d_o * SCALE), 0.8 * h_o * SCALE, -d_o * SCALE, 0, { color: "red" });
+  dda(ctx, 0.3 * h_o * SCALE + (-d_o * SCALE), 0.8 * h_o * SCALE, -d_o * SCALE, 0, { color: "red" });
+}
 
 /**
  * @param {CanvasRenderingContext2D} ctx 2d rendering context
  */
 const drawImage = (ctx) => {
   dda(ctx, -d_i * SCALE, 0, -d_i * SCALE, h_i * SCALE, { color: "blue" });
-  dda(ctx, -0.3 * h_i * SCALE + -d_i * SCALE, 0.8 * h_i * SCALE, 0.3 * h_i * SCALE + -d_i * SCALE, 0.8 * h_i * SCALE, {
-    color: "blue",
-  });
-  dda(ctx, -0.3 * h_i * SCALE + -d_i * SCALE, 0.8 * h_i * SCALE, -d_i * SCALE, h_i * SCALE, { color: "blue" });
-  dda(ctx, 0.3 * h_i * SCALE + -d_i * SCALE, 0.8 * h_i * SCALE, -d_i * SCALE, h_i * SCALE, { color: "blue" });
-  dda(ctx, -0.3 * h_i * SCALE + -d_i * SCALE, 0.8 * h_i * SCALE, -d_i * SCALE, 0, { color: "blue" });
-  dda(ctx, 0.3 * h_i * SCALE + -d_i * SCALE, 0.8 * h_i * SCALE, -d_i * SCALE, 0, { color: "blue" });
-};
+  dda(ctx, (-0.3 * h_i * SCALE) + (-d_i * SCALE), 0.8 * h_i * SCALE, 0.3 * h_i * SCALE + (-d_i * SCALE), 0.8 * h_i * SCALE, { color: "blue" });
+  dda(ctx, (-0.3 * h_i * SCALE) + (-d_i * SCALE), 0.8 * h_i * SCALE, -d_i * SCALE, h_i * SCALE, { color: "blue" });
+  dda(ctx, 0.3 * h_i * SCALE + (-d_i * SCALE), 0.8 * h_i * SCALE, -d_i * SCALE, h_i * SCALE, { color: "blue" });
+  dda(ctx, (-0.3 * h_i * SCALE) + (-d_i * SCALE), 0.8 * h_i * SCALE, -d_i * SCALE, 0, { color: "blue" });
+  dda(ctx, 0.3 * h_i * SCALE + (-d_i * SCALE), 0.8 * h_i * SCALE, -d_i * SCALE, 0, { color: "blue" });
+}
 
 /**
  * @param {CanvasRenderingContext2D} ctx 2d rendering context
@@ -284,7 +225,7 @@ const drawFocal = (ctx) => {
   if (sim === -1) {
     ctx.fillRect(X_ORIGIN + f * SCALE - 2, Y_ORIGIN - 2, 4, 4);
   }
-};
+}
 
 /**
  * @param {CanvasRenderingContext2D} ctx 2d rendering context
@@ -296,7 +237,7 @@ const drawCurvature = (ctx) => {
   if (sim === -1) {
     ctx.fillRect(X_ORIGIN + f * 2 * SCALE - 2, Y_ORIGIN - 2, 4, 4);
   }
-};
+}
 
 window.addEventListener("resize", () => {
   CANVAS_WIDTH = window.innerWidth;
@@ -308,52 +249,9 @@ window.addEventListener("resize", () => {
   canvas.height = CANVAS_HEIGHT;
 
   update();
-});
+})
 
-const update = () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  const menu = document.getElementById("checkbox-menu");
-  const label = document.querySelector(".menu > label");
-  const buttonsContainerEl = document.getElementsByClassName("buttons-container");
-  if (menu.checked) {
-    buttonsContainerEl[0].style.display = "flex";
-    label.innerHTML = "x";
-  } else {
-    buttonsContainerEl[0].style.display = "none";
-    label.innerHTML = "=";
-  }
-
-  const selectedSim = document.querySelector("input[name='sim']:checked");
-  const selectedType = document.querySelector("input[name='type']:checked");
-  sim = Number(selectedSim.value);
-  type = Number(selectedType.value);
-
-  f = Number(selectedType.value) * Number(focalLength.value);
-  d_o = Number(objDistance.value);
-  h_o = Number(objHeight.value);
-
-  d_i = 1 / (1 / f - 1 / (Number(selectedSim.value) * d_o));
-  h_i = (-d_i / (Number(selectedSim.value) * d_o)) * h_o;
-
-  drawBase(ctx);
-  drawObject(ctx);
-  drawImage(ctx);
-  drawFocal(ctx);
-  drawCurvature(ctx);
-  drawLabels(ctx);
-
-  if (Number(selectedSim.value) === 1) {
-    drawRay(ctx);
-  } else {
-    drawRayLens(ctx);
-  }
-};
-
-/**
- * @param {CanvasRenderingContext2D} ctx 2d rendering context
- */
-const drawRay = (ctx) => {
+const drawMirrorRay = (ctx) => {
   const objX = -d_o * SCALE;
   const objY = h_o * SCALE;
   const imgX = -d_i * SCALE;
@@ -364,50 +262,37 @@ const drawRay = (ctx) => {
   if (f > 0) {
     dda(ctx, -CANVAS_WIDTH / 2, objY, 0, objY, { color: "green" });
     ddaRay(ctx, 0, objY, focalPos, 0, { color: "green" });
-    if (h_i > 0) {
+    if (d_i < 0) {
       ddaRay(ctx, 0, objY, imgX, imgY, { color: "green", style: "dashed" });
     }
   } else {
     dda(ctx, -CANVAS_WIDTH / 2, objY, 0, objY, { color: "green" });
-    ddaRay(ctx, 0, objY, focalPos, 0, { color: "green", isVirtual: true, style: "dashed" });
-    ddaRayMod(ctx, focalPos, 0, 0, objY, { color: "green", style: "dashed" });
+    ddaRay(ctx, 0, objY, focalPos, 0, { color: "green", style: "dashed" });
+    ddaRay(ctx, 0, objY, focalPos, 0, { color: "green", negative: true });
   }
-
-  //rule 2
-  // ddaRay(ctx, objX, objY, 2*focalPos, 0, {color:"orange", isVirtual:true});
-  // if (h_i < 0) {
-  //   ddaRay(ctx, imgX, imgY, 2*focalPos, 0, {color:"orange", isVirtual:false});
-  // } else {
-  //   ddaRay(ctx, imgX, imgY, 2*focalPos, 0, {color:"orange", isVirtual:true});
-  // }
 
   // rule 3
-  if (f !== d_o) {
-    dda(ctx, -CANVAS_WIDTH / 2, imgY, 0, imgY, { color: "purple" });
-    if (h_i < 0) {
-      ddaRay(ctx, 0, imgY, focalPos, 0, { color: "purple" });
-    } else if (h_i > 0) {
-      dda(ctx, 0, imgY, CANVAS_WIDTH / 2, imgY, { color: "purple", style: "dashed" });
-      if (f > 0) {
-        ddaRay(ctx, 0, imgY, focalPos, 0, { color: "purple", isVirtual: true });
+  if (f > 0) {
+    if (d_o !== f) {
+      dda(ctx, -CANVAS_WIDTH / 2, imgY, 0, imgY, { color: "purple" });
+      dda(ctx, focalPos, 0, 0, imgY, { color: "purple" });
+      if (d_i > 0) {
+        ddaRay(ctx, focalPos, 0, objX, objY, { color: "purple" });
       } else {
-        ddaRay(ctx, 0, imgY, objX, objY, { color: "purple" });
+        dda(ctx, CANVAS_WIDTH / 2, imgY, 0, imgY, { color: "purple", style: "dashed" });
+        ddaRay(ctx, objX, objY, focalPos, 0, { color: "purple" });
       }
     }
+  } else {
+    if (d_o !== 0) {
+      dda(ctx, -CANVAS_WIDTH / 2, imgY, 0, imgY, { color: "purple" });
+      dda(ctx, CANVAS_WIDTH / 2, imgY, 0, imgY, { color: "purple", style: "dashed" });
+      ddaRay(ctx, 0, imgY, objX, objY, { color: "purple" });
+    }
   }
+}
 
-  // rule 4
-  // if (d_o !== 0) {
-  //   ddaRay(ctx, 0, 0, objX, objY, {color:"teal"});
-  //   if (d_i >= 0) {
-  //     ddaRay(ctx, 0, 0, imgX, imgY, {color:"teal"});
-  //   } else {
-  //     // ddaRay(ctx, 0, 0, -d_i*SCALE, h_i*SCALE, {color:"teal", style:"dashed"});
-  //   }
-  // }
-};
-
-const drawRayLens = (ctx) => {
+const drawLensRay = (ctx) => {
   const objX = -d_o * SCALE;
   const objY = h_o * SCALE;
   const imgX = -d_i * SCALE;
@@ -415,41 +300,39 @@ const drawRayLens = (ctx) => {
   const focalPos = -f * SCALE;
 
   // rule 1
-  if (f !== d_o) {
+  if (type === 1) {
     dda(ctx, -CANVAS_WIDTH / 2, objY, 0, objY, { color: "green" });
-    if (type === -1) {
-      ddaRay(ctx, 0, objY, focalPos, 0, { color: "green", isVirtual: true });
-      // ddaRay(ctx, 0, objY, imgX, imgY, {color:"green", isVirtual:true});
-    } else {
-      ddaRay(ctx, 0, objY, focalPos, 0, { color: "green", isVirtual: true });
-      ddaRay(ctx, focalPos, 0, 0, objY, { color: "green" });
+    ddaRay(ctx, 0, objY, focalPos, 0, { color: "green", style: "dashed" });
+    ddaRay(ctx, 0, objY, focalPos, 0, { color: "green", negative: true });
+  } else if (type === -1) {
+    dda(ctx, -CANVAS_WIDTH / 2, objY, 0, objY, { color: "green" });
+    ddaRay(ctx, 0, objY, focalPos, 0, { color: "green" });
+    if (d_i > 0 && d_o !== -f) {
+      ddaRay(ctx, 0, objY, imgX, imgY, { color: "green", style: "dashed" });
     }
   }
 
   // rule 2
-  // ddaRay(ctx, objX, objY, 0, 0, {color:"teal", isVirtual:true});
-  // ddaRay(ctx, imgX, imgY, 0, 0, {color:"teal", isVirtual:true});
+  ddaRay(ctx, 0, 0, objX, objY, { color: "teal", negative: true });
+  ddaRay(ctx, 0, 0, objX, objY, { color: "teal" });
 
   // rule 3
-  if (type === -1) {
-    if (d_o !== 0) {
+  if (d_o !== 0 && d_o !== -f) {
+    if (type === 1) {
       dda(ctx, CANVAS_WIDTH / 2, imgY, 0, imgY, { color: "purple" });
-      ddaRay(ctx, 0, imgY, -focalPos, 0, { color: "purple" });
+      dda(ctx, -CANVAS_WIDTH / 2, imgY, 0, imgY, { color: "purple", style: "dashed" });
+      ddaRay(ctx, 0, imgY, objX, objY, { color: "purple" });
+    } else {
+      dda(ctx, -focalPos, 0, 0, imgY, { color: "purple" });
+      ddaRay(ctx, -focalPos, 0, objX, objY, { color: "purple" });
       if (d_i > 0) {
-        dda(ctx, -CANVAS_WIDTH / 2, imgY, 0, imgY, { color: "purple" });
+        dda(ctx, -CANVAS_WIDTH / 2, imgY, 0, imgY, { color: "purple", style: "dashed" });
+      } else {
+        dda(ctx, CANVAS_WIDTH / 2, imgY, 0, imgY, { color: "purple" });
       }
     }
-  } else {
-    if (d_o !== 0) {
-      dda(ctx, CANVAS_WIDTH / 2, imgY, 0, imgY, { color: "purple" });
-      dda(ctx, -CANVAS_WIDTH / 2, imgY, 0, imgY, { color: "purple" });
-      ddaRay(ctx, 0, imgY, objX, objY, { color: "purple" });
-    } else if (d_o === 0) {
-      dda(ctx, CANVAS_WIDTH / 2, objY, 0, objY, { color: "purple" });
-      dda(ctx, -CANVAS_WIDTH / 2, objY, 0, objY, { color: "purple" });
-    }
   }
-};
+}
 
 const drawLabels = (ctx) => {
   const objX = X_ORIGIN - d_o * SCALE;
@@ -480,6 +363,48 @@ const drawLabels = (ctx) => {
   if (sim === -1) {
     ctx.fillText("Kurvatur", X_ORIGIN + f * 2 * SCALE, Y_ORIGIN);
   }
-};
+}
+
+const update = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  const menu = document.getElementById('checkbox-menu');
+  const label = document.querySelector('.menu > label');
+  const buttonsContainerEl = document.getElementsByClassName('buttons-container');
+  if (menu.checked) {
+    buttonsContainerEl[0].style.display = "flex";
+    label.innerHTML = "x";
+  } else {
+    buttonsContainerEl[0].style.display = "none";
+    label.innerHTML = "=";
+  }
+
+  const selectedSim = document.querySelector("input[name='sim']:checked");
+  const selectedType = document.querySelector("input[name='type']:checked");
+  sim = Number(selectedSim.value);
+  type = Number(selectedType.value);
+
+  f = Number(selectedType.value) * Number(focalLength.value);
+  d_o = Number(objDistance.value);
+  h_o = Number(objHeight.value);
+
+  d_i = 1 / (1 / f - 1 / (Number(selectedSim.value) * d_o));
+  h_i = (-d_i / (Number(selectedSim.value) * d_o)) * h_o;
+
+  drawBase(ctx);
+  drawObject(ctx);
+  drawImage(ctx);
+  drawFocal(ctx);
+  drawCurvature(ctx);
+  drawLabels(ctx);
+  drawCircle(ctx, X_ORIGIN - f * 2 * SCALE, Y_ORIGIN, f * 2 * SCALE);
+
+  if (Number(selectedSim.value) === 1) {
+    drawMirrorRay(ctx);
+  } else {
+    drawLensRay(ctx);
+  }
+}
 
 update();
+
